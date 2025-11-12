fcaf6c8fe1453bb8f6e6500f3065b663cf53c4f4
shatha2 <shatha.sa38@gmail.com>
2025-11-12 10:06:59 +0300

    chore: add colors file to utils

---
+++ src/utils/colors/Colors.js
@@ -0,0 +1,12 @@
+export default {
+  // ألوان المشروع
+  colors: {
+    primary: '#8E221F',        // اللون الرئيسي 
+    secondary: '#C32729',      // اللون الثانوي 
+    accent: '#FECE5D',         // لون مساعد 
+    accentLight: '#FFEFEE',    // لون مساعد فاتح 
+    
+    text: '#000000',           // لون النص
+    background: '#FFFFFF',     // لون الخلفية
+  }
+};
