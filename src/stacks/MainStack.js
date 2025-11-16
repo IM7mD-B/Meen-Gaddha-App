76bc7e6b8ab16aa64d018f781dcc57ae88e9dcdc
Mohammed.B <mbawdood3@gmail.com>
2025-11-16 15:26:03 +0300

    feat: add GameInstructions onboarding screen

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -24,7 +24,7 @@ const MainStack = () => {
         }}        // initialRouteName="Login"
         initialRouteName="Splash"
       >
-
+        <Stack.Screen name="GameInstructions" component={GameInstructions} />
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="GameInstructions" component={GameInstructions} />
         <Stack.Screen name="GameSetting" component={GameSetting} />
