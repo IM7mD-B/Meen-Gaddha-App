514aac4bf590355130d374b7b223f965bf06d98e
shatha2 <shatha.sa38@gmail.com>
2025-11-16 12:04:19 +0300

    "feat: add bottom tab navigation
    - Installed bottom tab library
    - Added icons for bottom tabs
    - Updated main stack to integrate bottom tabs"

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -24,11 +24,8 @@ const MainStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          animation: 'none',
-        }}        // initialRouteName="Login"
-        initialRouteName="Splash"
+        screenOptions={{ headerShown: false  }}
+        initialRouteName="Home"
       >
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="GameInstructions" component={GameInstructions} />
