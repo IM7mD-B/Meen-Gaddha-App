4990167bcd4632fa19c288bf76404e1f4eb32aca
Mohammed.B <mbawdood3@gmail.com>
2025-11-18 08:52:24 +0300

    chore: enhance app icon and splash screen

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -24,7 +24,6 @@ const MainStack = () => {
         }}        // initialRouteName="Login"
         initialRouteName="Splash"
       >
-        <Stack.Screen name="GameInstructions" component={GameInstructions} />
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="GameInstructions" component={GameInstructions} />
         <Stack.Screen name="GameSetting" component={GameSetting} />
