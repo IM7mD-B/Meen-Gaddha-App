0dcc5c8eb70c5b4fce4df92c7e5c1ac3818c245c
shatha2 <shatha.sa38@gmail.com>
2025-12-04 06:54:25 +0300

    feat: add CategoryCard, Game screen, reuse custom header, and update Main Stack; chore: install react-native-orientation-locker

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -22,8 +22,11 @@ const MainStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        screenOptions={{ headerShown: false  }}
-        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+          animation: 'none',
+        }}        // initialRouteName="Login"
+        initialRouteName="GameScreen"
       >
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="GameInstructions" component={GameInstructions} />
