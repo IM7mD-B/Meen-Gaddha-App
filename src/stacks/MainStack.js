89fd124363e1d45481b44462bebb9a30365d4860
shatha2 <shatha.sa38@gmail.com>
2025-12-07 11:56:08 +0300

    feat(home): update HomeScreen and MainStack, add new API files

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/auth/LoginScreen';
+import HomeScreen from '../screens/homeScreen/HomeScreen';
 import BottomTab from '../components/shared/BottomTabs';
 
 const Stack = createNativeStackNavigator();
@@ -11,13 +12,16 @@ const MainStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        screenOptions={{ headerShown: false  }}
+        screenOptions={{
+          headerShown: false,
+          animation: 'none',
+        }}        // initialRouteName="Login"
         initialRouteName="Home"
       >
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={BottomTab} />
 
-  
+
 
       </Stack.Navigator>
     </NavigationContainer>
