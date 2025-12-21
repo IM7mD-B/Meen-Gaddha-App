b07795fc1a07c5aa56947246781eb4269164c3e4
shatha2 <shatha.sa38@gmail.com>
2025-12-21 10:13:43 +0300

    feat: configure axios instance for API requests

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,11 +1,39 @@
 import axios from 'axios';
 import baseURL from './baseURL';
+import useAuthStore from '../store/AuthStore'; 
 
 const api = axios.create({
-    baseURL,
-    headers: {
+  baseURL,
+  headers: {
     'Content-Type': 'application/json',
   },
-})
+});
+
+// Request Interceptor
+api.interceptors.request.use(
+  (config) => {
+    const token = useAuthStore.getState().token;
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Response Interceptor 
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      // انتهت الجلسة أو توكن غير صالح
+      const logout = useAuthStore.getState().logout;
+      await logout();
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
