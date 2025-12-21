9c3afb3dacaf995fef5cbc51958d31101f492790
Mohammed.B <mbawdood3@gmail.com>
2025-12-21 10:19:44 +0300

    chore: update app icon

--- src/screens/auth/WelcomeScreen.js
+++ src/screens/auth/WelcomeScreen.js
@@ -39,12 +39,6 @@ const WelcomeScreen = ({ navigation }) => {
                     <Text style={[styles.guestText, { color: colors.colors.secondary }]}>الدخول كضيف</Text>
                 </TouchableOpacity>
             </View>
-            <View>
-                <TouchableOpacity onPress={() => navigation.navigate('ChangePass')}>
-                    <Text style={[styles.guestText, { color: colors.colors.secondary }]}>الدخول كضيف</Text>
-                </TouchableOpacity>
-            </View>
-         
         </View>
     )
 }
